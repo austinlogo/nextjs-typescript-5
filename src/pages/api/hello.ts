210a9ea2c3db220ca80ef9178672e11ead52e6b0
logo <austinlg@vt.edu>
2023-04-13 18:08:41 -0400

    create initial typescript decorator on class

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { log } from "console";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
+  name: string;
+};
+
+function loggedMethod(originalMethod: any, _context: any) {
+  function replacementMethod(this: any, ...args: any[]) {
+    console.log("hit");
+    const result = originalMethod.call(this, ...args);
+    return result;
+  }
+  return replacementMethod;
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: new Test().myMethod() });
+}
+
+class Test {
+  @loggedMethod
+  myMethod() {
+    return "hello";
+  }
 }
